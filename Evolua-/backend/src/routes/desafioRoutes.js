a45c8a61b56c58723b209e9895f1c140276eaa2e
Carlos150806 <Carlos.eduardo15080622@gmail.com>
2026-03-20 19:44:36 -0300

    Correção de duplicidades

--- Evolua-/backend/src/routes/desafioRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const desafioController = require('../controllers/desafioController');
-
-router.get('/', desafioController.listar);
-router.post('/', desafioController.criar);
-
-module.exports = router;
