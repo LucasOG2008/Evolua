cfe206dfc95f8d9573694392d3a1b24eb3d77626
MuriloMiranda65 <mirandamurilosilva@gmail.com>
2026-04-14 18:04:44 -0300

    Mostrar ou Ususuario ou Psicologo

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,6 +5,8 @@ app.get("/", (req, res) => {
     res.send("Servidor funcionando!");
 });
 
-app.listen(process.env.PORT || 3000, () => {
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
     console.log("Servidor rodando na porta 3000");
 });
