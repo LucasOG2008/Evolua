e19b0d892e21343702ed754be47b0e6334bee86c
MuriloMiranda65 <mirandamurilosilva@gmail.com>
2026-04-16 19:15:57 -0300

    rota de cadastro

---
+++ Js/cadastro.js
@@ -0,0 +1,32 @@
+const form = document.getElementById("acesso");
+
+form.addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    const dados = {
+        nome: document.getElementById("usuario").value,
+        cargo: document.getElementById("cargo").value,
+        setor: document.getElementById("setor").value,
+        email: document.getElementById("email").value,
+        cpf: document.getElementById("cpf").value,
+        senha: document.getElementById("senha").value
+    };
+
+    try {
+        const resposta = await fetch("http://localhost:3000/auth/cadastrar", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(dados)
+        });
+
+        if (resposta.ok) {
+            alert("Cadastrado com sucesso!");
+        }
+
+    } catch (erro) {
+        console.error("Erro:", erro);
+        alert("Erro ao conectar com o servidor");
+    }
+});
